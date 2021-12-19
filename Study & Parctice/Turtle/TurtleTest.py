6f390af1787dada723e4875ad36d686ef4cfe026
galaxy0524yang <59867030+galaxy0524yang@users.noreply.github.com>
2021-12-19 22:44:08 +0900

    Global を追加

--- Study & Parctice/Turtle/TurtleTest.py
+++ Study & Parctice/Turtle/TurtleTest.py
@@ -4,20 +4,21 @@ t.tracer(False)
 a, b, c = t.Pen(), t.Pen(), t.Pen()
 NumT = 0
 
-def try1(d, ang, Num):
-    for x in range(100 * (Num + 1)):
+def try1(d, ang):
+    global NumT
+    for x in range(100 * (NumT + 1)):
         d.forward(x)
         d.left(ang)
-        Num += 1
+    NumT += 1
 
 a.color("#008000")
 try1(a, 39, NumT)
-NumT += 1
+# NumT += 1
 b.color("#808000")
 try1(b, 59, NumT)
-NumT += 1
+# NumT += 1
 c.color("#800000")
 try1(c, 99, NumT)
-NumT += 1
+# NumT += 1
 
 t.done()
