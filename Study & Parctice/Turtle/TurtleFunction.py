fc8b87ac4ddedeb2cbe3909c0ff72fb962713256
Tetsuyu <v-tetsuyuyo@microsoft.com>
2021-12-27 13:46:21 +0900

    turtlepractice

--- Study & Parctice/Turtle/TurtleFunction.py
+++ Study & Parctice/Turtle/TurtleFunction.py
@@ -1,15 +1,17 @@
 from turtle import *
 
 def T_Func1(a1, angle):
+    pen1 = Pen()
     for a in range(a1):
-        forward(a1)
-        left(angle)
+        pen1.forward(a)
+        pen1.left(angle)
 
 def T_Func2(a2):
+    pen2 = Pen()
     for a in range(a2):
-        forward(a2)
+        pen2.forward(a)
         if a2 % 2 == 1:
-            left(90)
+            pen2.left(90)
         else:
-            right(90)
+            pen2.right(90)
 
--- Study & Parctice/Turtle/turtle_action_4.py
+++ Study & Parctice/Turtle/turtle_action_4.py
@@ -1,4 +1,14 @@
 import turtle as t
 import TurtleFunction
 
+t.color("#FF0000")
+t.tracer(False)
+for a in range(300):
+    t.forward(a)
+    t.left(a)
 
+
+i = 500
+TurtleFunction.T_Func1(i, 95)
+
+t.done()
