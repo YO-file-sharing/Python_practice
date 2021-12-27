8f81caf7ad9992a24e54671ad7c4c50f5308f6d1
Tetsuyu <v-tetsuyuyo@microsoft.com>
2021-12-27 19:08:14 +0900

    1

--- Study & Parctice/study04.py
+++ Study & Parctice/study04.py
@@ -1,13 +1,18 @@
 import random as r
+from turtle import xcor
 
-def GuessNum():
+def GuessNum(X):
+    global Q1, Q2
     Q1 = Q2 = 0
-    for a in range(10000000) :
+    for a in range(X) :
         Num = r.randint(0,1)
         if Num == 0:
             Q1 += 1
         else:
             Q2 += 1
-    print(Q1, Q2)
+    return Q1, Q2
 
-GuessNum()
+
+GuessNum(5000)
+print(Q1)
+print(Q2)
